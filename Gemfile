f79a118b9d4ae3ee8acfd44c7da3bf355f339172
Didi <diotanara@gmail.comgit config --global user.name Didi>
2016-12-15 16:50:31 +0800

    Finish user Signup

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # For secure password
 gem 'bcrypt', '3.1.11'
+# Bootstap for the purpose of display
+gem 'bootstrap-sass', '3.3.6'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
+  post '/signup', to: 'users#create'
+
   root 'static_pages#home'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
